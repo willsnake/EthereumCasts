d575d1a86d85d99e5bb8a88829dd01aaa2386438
Daniel Rodríguez Monroy <drodriguez@itexico.net>
2018-04-20 16:58:56 -0500

    Updated inbox constructor

--- inbox/contracts/Inbox.sol
+++ inbox/contracts/Inbox.sol
@@ -2,12 +2,13 @@ pragma solidity ^0.4.17;
 
 contract Inbox {
     string public message;
-
-    function Inbox(string initialMessage) public {
+    
+    constructor(string initialMessage) public {
         message = initialMessage;
     }
     
     function setMessage(string newMessage) public {
         message = newMessage;
     }
+
 }
